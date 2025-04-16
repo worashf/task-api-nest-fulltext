19788a21a5e2036d07e0870ecefdec1ce1e4c6f2
worashf <uppert83@gmail.com>
2025-04-16 13:16:32 +0300

    Import module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available app-wide
+      envFilePath: '.env', // Specify .env file
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USER', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'postgres'),
+        database: configService.get<string>('DB_NAME', 'task_management'),
+        autoLoadEntities: true,
+        synchronize: false, // Set to false to use migrations in production
+        migrations: ['dist/migrations/*.js'],
+        migrationsRun: true, // Automatically run migrations on startup
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
