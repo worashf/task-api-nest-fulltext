e71c55703e0552e89bde0df2b4ce20cc8c1090e7
worashf <uppert83@gmail.com>
2025-04-16 13:14:23 +0300

    Add  user files

---
+++ src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+
+import {Task} from "src/tasks/task.entity";
+@Entity("users")
+export class  User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ unique: true })
+    username!: string;
+
+    @Column()
+    password!: string;
+
+    @OneToMany(() => Task, (task) => task.assignedTo)
+    tasks?: Task[]
+
+}
